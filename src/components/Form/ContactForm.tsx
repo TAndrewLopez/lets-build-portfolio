d620905485d5e5384a2787d13f90c7f9a31ad211
Andrew Dobson <tandrewlopez@gmail.com>
2023-02-22 14:17:14 -0500

    updated bot field

--- src/components/Form/ContactForm.tsx
+++ src/components/Form/ContactForm.tsx
@@ -65,7 +65,7 @@ const ContactForm = () => {
           name="contact"
           method="POST"
           data-netlify="true"
-          data-netlify-honeypot="bot-field"
+          netlify-honeypot="bot-field"
           onSubmit={handleSubmit}>
           <input type="hidden" name="form-name" value="contact" />
           <input name="bot-field" type="hidden" />
