1b190a7f2ba9b4fb0c3947e106b7eea2c81b9813
Andrew Dobson <tandrewlopez@gmail.com>
2023-02-22 14:13:49 -0500

    added spam prevention

--- src/components/Form/ContactForm.tsx
+++ src/components/Form/ContactForm.tsx
@@ -65,8 +65,10 @@ const ContactForm = () => {
           name="contact"
           method="POST"
           data-netlify="true"
+          data-netlify-honeypot="bot-field"
           onSubmit={handleSubmit}>
           <input type="hidden" name="form-name" value="contact" />
+          <input name="bot-field" type="hidden" />
           <div className="grid w-full gap-4 py-2 md:grid-cols-2">
             <div className="flex flex-col">
               <label className="py-2 text-sm uppercase">Name</label>
